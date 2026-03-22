f7c5c67e80795d9f84e632588b463eb6fdb548b2
katdzy <andreidungca6@gmail.com>
2026-03-22 20:15:30 +0800

    supabase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Supabase.initialize(
-    url: 'https://vrfufkaxzcwrumcjyjbb.supabase.co',
-    anonKey: 'sb_publishable_HD-0sQN57QlroY4foX-tCQ_LSXJCCR-',
+    url: 'https://xsayqmmvzpkfkkvodxsx.supabase.co',
+    anonKey: 'sb_publishable_aLkOb3P2eAWa7WoteCWkjw_lIpIQamu',
   );
 
   runApp(const HauMonstersApp());
